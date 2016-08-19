250b9c6ebfb4cee04d113fbf67cbda7348780490
Brendon Butler <bbutler452@gmail.com>
2016-08-19 01:56:02 -0400

    HelloWorld Program Prints out "Hello World" to console Added comments

---
+++ src/HelloWorld.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Brendon Butler on 8/19/2016.
+ */
+public class HelloWorld {
+
+	public static void main(String[] args) { // mandatory method (program starts here)
+		System.out.println("Hello World"); // prints "Hello World" to console
+	}
+}
